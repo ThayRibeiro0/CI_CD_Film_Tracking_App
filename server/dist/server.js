781931318f6f65be70f60fdaf76ead829d130643
Thays Moia Ribeiro <thaysmoiaribeiro@MacBookPro.lan>
2025-05-16 13:41:05 -0700

    Set up app to deploy

---
+++ server/dist/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+// import path from 'node:path';
+import db from './config/connection.js';
+import routes from './routes/index.js';
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3003;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serves static files in the entire client's dist folder
+app.use(express.static('../client/dist'));
+app.use(routes);
+db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
